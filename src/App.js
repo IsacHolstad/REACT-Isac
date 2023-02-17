1932983009f8347dbc264dfda79eb09812323005
IsacHolstad <wisac22@gmail.com>
2023-02-17 23:52:40 +0100

    counter hooks made and buttons are working ep.6 -6:11

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Counter from "./Counter";
+import CounterHooks from "./CounterHooks";
 
 function App() {
   return  (
-      <Counter initialCount={0}></Counter>
-
+      <>
+        <Counter initialCount={0}></Counter>
+        Counter Hooks
+        <CounterHooks initialCount={0}></CounterHooks>
+      </>
   )
 }
 
